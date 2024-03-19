67a0ff0ca386c1a93b967b2afc87e928d4972574
oopfjp <112739266+oopfjp@users.noreply.github.com>
2024-03-19 09:06:36 +0800

    Update mvtec_embedding.py

--- Data/mvtec_embedding.py
+++ Data/mvtec_embedding.py
@@ -36,13 +36,6 @@ if __name__ == "__main__":
     Dataset = MvTec()
     loader = DataLoader(Dataset, batch_size=64)
     a=1
-    # val_Dataset = MvTec(args, mode='val')
-    # # label, file = Dataset[0]
-    # train_dataLoader = DataLoader(train_Dataset, batch_size=64)
-    # for idx, (target, input) in enumerate(train_dataLoader):
-    #     print(idx)
-    #     a = 1
-    # a = 1
-
+   
 
 
